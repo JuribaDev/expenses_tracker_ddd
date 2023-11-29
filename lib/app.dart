12519612462538096066fb1834789cc7144a4078
Juriba <Juribaalsiari@gmail.com>
2023-11-29 11:15:24 +0300

    clean up

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,4 @@
 import 'package:expenses_tracker_ddd/config/theme/color_scheme.dart';
-import 'package:expenses_tracker_ddd/l10n/l10n.dart';
 import 'package:expenses_tracker_ddd/presentation/expense/screen/expense_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -9,8 +8,6 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
       themeMode: ThemeMode.dark,
       theme: ThemeData(colorScheme: lightColorScheme),
       darkTheme: ThemeData(colorScheme: darkColorScheme),
